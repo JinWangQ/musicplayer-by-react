a168b2e1936374d981694fe3db97e928270a0602
Jin <wangatqueens@gmail.com>
2018-02-08 00:08:03 -0500

    Initial commit

---
+++ app/components/hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './hello.less'
+
+
+class Hello extends React.Component {
+	render() {
+		return <div className="hello-component">
+				Hello, React and webpack
+			</div>;
+	}
+}
+export default Hello;
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: [
+		'webpack-dev-server/client?http://localhost:3000',
+		'webpack/hot/only-dev-server',
+		'react-hot-loader/patch',
+		path.join(__dirname, 'app/index.js')
+	],
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: '[name].js',
+		publicPath: '/'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.tpl.html',
+			inject: 'body',
+			filename: './index.html'
+		}),
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development')
+		})
+	],
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: ['@babel/preset-env', '@babel/react']
+				}
+			}
+		}, {
+			test: /\.css$/,
+			use: [{
+				loader: "style-loader"
+			}, {
+				loader: "css-loader"
+			}]
+		}, {
+			test: /\.less$/,
+			use: [{
+				loader: "style-loader" // creates style nodes from JS strings
+			}, {
+				loader: "css-loader" // translates CSS into CommonJS
+			}, {
+				loader: "less-loader" // compiles Less to CSS
+			}]
+		}]
+	}
+}
