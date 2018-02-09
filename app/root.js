731979cbb381e2f1bfd66eaaf14ff4f3b1bbd4eb
Jin <wangatqueens@gmail.com>
2018-02-09 00:06:12 -0500

    progress_init added

--- app/root.js
+++ app/root.js
@@ -1,10 +1,45 @@
 import React from 'react'
 import Header from './components/header'
+import Progress from './components/progress'
 
 class Root extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			progress: '-'
+		};
+	}
+	componentDidMount() {
+		$("#player").jPlayer({
+			ready: function() {
+				$(this).jPlayer("setMedia", {
+					mp3: "http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3"
+				}).jPlayer('play');
+			},
+			supplied: "mp3",
+			wmode: "window",
+			useStateClassSkin: true
+		});
+
+		$("#player").bind($.jPlayer.event.timeupdate, (e) => {
+			this.setState({
+				progress: Math.round(e.jPlayer.status.currentTime)
+			});
+		});
+	}
+
+
+
 	render() {
 		return (
-			<Header />
+			<div>
+				<Header /> 
+				<Progress 
+				progress = {this.state.progress} >
+
+				</Progress>
+			</div>
+
 		);
 	}
 }
