b7985026221c4818e8f9cc5c8b723b60a3cec835
Jin <wangatqueens@gmail.com>
2018-02-09 22:49:16 -0500

    progress bar & UI update

--- app/root.js
+++ app/root.js
@@ -1,43 +1,40 @@
 import React from 'react'
 import Header from './components/header'
-import Progress from './components/progress'
+import Player from './page/player'
+import {
+	MUSIC_LIST
+} from './config/musiclist'
+
 
 class Root extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			progress: '-'
+			currentMusicItem: MUSIC_LIST[0]
 		};
 	}
 	componentDidMount() {
 		$("#player").jPlayer({
 			ready: function() {
 				$(this).jPlayer("setMedia", {
-					mp3: "http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3"
+					mp3: "http://jplayer.org/audio/mp3/Miaow-07-Bubble.mp3"
 				}).jPlayer('play');
 			},
 			supplied: "mp3",
 			wmode: "window",
 			useStateClassSkin: true
 		});
-
-		$("#player").bind($.jPlayer.event.timeupdate, (e) => {
-			this.setState({
-				progress: Math.round(e.jPlayer.status.currentTime)
-			});
-		});
 	}
 
+	componentWillUnmount() {
 
+	}
 
 	render() {
 		return (
 			<div>
-				<Header /> 
-				<Progress 
-				progress = {this.state.progress} >
-
-				</Progress>
+				<Header />
+				<Player currentMusicItem={this.state.currentMusicItem}></Player> 
 			</div>
 
 		);
